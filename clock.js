4b61555d7501baad2ee160b5649772291c3d82b5
Choi sang hoon <tkdgnsdl91xp@naver.com>
2019-10-12 15:14:57 +0900

    feat : 요일 추가, clock함수 갱신 추가
    fix : 월, 일, 시간이 10보다 작을 경우 '0'을 붙여 출력하도록 수정

--- clock.js
+++ clock.js
@@ -5,13 +5,16 @@ function clock() {
     const main = new Date();
     const month = main.getMonth();
     const date = main.getDate();
+    const day = main.getDay();
+    const arrDay = ["월", "화", "수", "목", "금", "토", "일"];
     const hour = main.getHours();
     const min = main.getMinutes();
     const sec = main.getSeconds();
-    clockTitle.innerText = `${month}월 ${date}일 ${hour}시 ${min}분 ${sec}초`
+    clockTitle.innerText = `${month+1 < 10 ? `0${month+1}` : month+1}월 ${date < 10 ? `0${date}` : date}일 ${arrDay[day]}요일 ${hour < 10 ? `0${hour}` : hour}시 ${min < 10 ? `0${min}`:min}분${sec < 10 ? `0${sec}` : sec}초`;
 };
 
 function init() {
     clock();
+    setInterval(clock, 1000);
 };
 init();
